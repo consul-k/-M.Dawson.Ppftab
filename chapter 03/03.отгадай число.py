32d03ac67c011ab9eb461fa3492615d1079bb727
consul-k <138245769+consul-k@users.noreply.github.com>
2023-07-09 14:36:08 +0300

    Update 03.отгадай число.py
    улучшил код

--- chapter 03/03.отгадай число.py
+++ chapter 03/03.отгадай число.py
@@ -6,23 +6,19 @@ print("Постарайтесь отгадать его за 7 попыток.\n
 
 
 the_number = random.randint(1, 100)
-guess = int(input("Ваша догадка: "))
-tries = 1
-
+tries = 0
+guess = 0
 
 while guess != the_number and tries < 7:
+    guess = int(input("Ваша догадка: "))
     if guess > the_number:
         print("Меньше...")
     else:
         print("Больше...")
-            
-    guess = int(input("Ваша догадка: "))
     tries += 1
-    if tries == 7:
-        break
     
 if guess == the_number:
-    print("Вы угадали!  Это число было", the_number)
+    print("Вы угадали! Это число было", the_number)
     print("И это стоило всего", tries, "попыток!\n")
 else:
     print("Ты проиграл компьютеру, кожанный мешок! И это стоило тебе", tries, "попыток!")
