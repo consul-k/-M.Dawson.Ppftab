5d56db06310c327f594f96aa5933dbece5124e4e
consul-k <138245769+consul-k@users.noreply.github.com>
2023-07-09 15:24:20 +0300

    Update 04.число отгадывает компьютер.py
    улучшил код

--- chapter 03/04.число отгадывает компьютер.py
+++ chapter 03/04.число отгадывает компьютер.py
@@ -5,28 +5,30 @@ print("\nЯ загадываю компьютеру число между 1 и 1
 print("Компьютер пробует отгадать его за 7 попыток.\n")
 
 
-the_number = int(input("Мое число: "))
-guess = random.randint(1, 100)
-tries = 1
+the_number = int(input("Мое число от 1-100: "))
+guess = 0
+tries = 0
 
+g_min = 1
+g_max = 100
 
 while guess != the_number and tries < 7:
+    guess = random.randint(g_min, g_max)
     print("Компьютер: ", guess)
     if guess > the_number:
-        guess = random.randint(1, the_number)
-        input("Человек: Меньше... Даю тебе еще один шанс ")
-    else:
-        guess = random.randint(the_number, 100)
-        input("Человек: Больше... Даю тебе еще один шанс")
+        g_max = guess
+        print("Человек: Меньше... Даю тебе еще один шанс ")
+        
+    elif guess < the_number:
+        g_min = guess
+        print("Человек: Больше... Даю тебе еще один шанс") 
 
     tries += 1
-    if tries == 7:
-        break
     
 if guess == the_number:
-    print("\nСкайнет уже в пути!  Это число было", the_number)
-    print("И это стоило всего", tries, "попыток!\n")
+    print("\nСкайнет уже в пути! Это число было", the_number)
+    print("И я справился за", tries, "попыток!\n")
 else:
-    print("\nЖелезный дубина проиграл кожанному мешку! И это стоило тебе", tries, "попыток!")
+    print("\nЖелезный дубина проиграл кожанному мешку! И это стоило мне", tries, "попыток!")
   
 input("\n\nНажмите Enter, чтобы выйти.")
