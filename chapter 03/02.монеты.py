4cae8e242cd5543004fe8a24862dc51844bbdb11
consul-k <138245769+consul-k@users.noreply.github.com>
2023-07-09 14:18:10 +0300

    Update 02.монеты.py
    улучшил код

--- chapter 03/02.монеты.py
+++ chapter 03/02.монеты.py
@@ -1,7 +1,18 @@
 import random
 
 print("\tПеред вами симулятор бросания монетки 100 раз подряд\n")
-side1 = random.randint(1, 100)
-side2 = 100 - side1
+
+#орел
+side1 = 0
+#решка
+side2 = 0
+
+for i in range(100):
+    i = random.randint(0,1)
+    if i == 0:
+        side1 += 1
+    else:
+        side2 += 1
+
 print("\n\nЯ сто раз подбросил монетку и вот, что у меня вышло: ", side1, "орлов", side2, "решек\n")
 input("\n\nНажмите Enter, чтобы выйти.")
