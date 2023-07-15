9357bc0ba2dcef42023c712901de6d55b274982b
consul-k <138245769+consul-k@users.noreply.github.com>
2023-07-15 11:47:49 +0300

    Create 01. отгадай число.py

---
+++ chapter 06/01. отгадай число.py
@@ -0,0 +1,63 @@
+'''
+  
+Доработайте функцию ask_number() так, чтобы ее можно было вызывать еще с одним параметром -
+кратностью (величиной шага). Сделайте шаг по умолчанию равным 1.
+  
+'''
+  
+
+import random  
+
+#инструкция
+
+def display_instruct():
+    print("\tДобро пожаловать в 'Отгадай число'!",
+          "\nЯ думаю о числе между 1 и 100...",
+          "\nПостарайтесь отгадать его за 7 попыток.\n")
+
+#создаем загаданное число
+def the_number():
+    global number
+    number = random.randint(1, 101)
+    return(number)
+
+tries = 1
+guess = 0
+
+#спрашиваем число
+def ask_number():
+    global guess
+    global tries
+    guess = int(input("Ваша догадка: "))
+    while guess != number or tries < 7:
+        if guess == number:
+            break
+        elif guess > number:
+            print("Меньше...")
+        else:
+            print("Больше...")
+        guess = int(input("Ваша догадка: "))
+        tries += 1
+        if tries == 7:
+            break
+
+    return guess
+
+def congrat():
+    
+    if guess == number:
+        print("Вы угадали!  Это число было ", number,
+        ", и это стоило всего", tries, "попыток!\n")
+    else:
+        print("Ты проиграл компьютеру, кожанный мешок! И это стоило тебе", tries, "попыток!")
+
+
+def main():
+    display_instruct()
+    number = the_number()
+    ask_number()
+    congrat()
+    
+
+main()
+input("\n\nНажмите Enter, чтобы выйти.")
